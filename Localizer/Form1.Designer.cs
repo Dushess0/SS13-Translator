f0c87018547d0d04780414b7cba0e91ab514c968
Dushess0 <ilyabannila@gmail.com>
2020-08-19 09:47:06 +0200

    ver 0.7

--- Localizer/Form1.Designer.cs
+++ Localizer/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,7 +45,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(104, 197);
+            this.button1.Location = new System.Drawing.Point(13, 197);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -60,22 +61,31 @@
             this.groupBox1.Size = new System.Drawing.Size(259, 178);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(6, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(163, 197);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Обновить список";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.progressBar1);
@@ -93,6 +103,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
 
 
     }
--- Localizer/Form1.cs
+++ Localizer/Form1.cs
@@ -15,36 +15,70 @@ namespace Localizer
         //download list of files from repo
         //download files from repo
         //replace files in directory
-
         private string[] files;
-        private const string listFile = "listfile";
-        string remoteUri = "https://raw.githubusercontent.com/Dushess0/SS13-Translator/master/";
+        private const string listFile = "filelist";
+        string downloadBase = "https://raw.githubusercontent.com/Dushess0/SS13-Translator/master/";
         public Form1()
         {
             InitializeComponent();
         }
         private void Download(string filename)
         {
-            var webclient = new WebClient();
-            webclient.DownloadFile(remoteUri+filename, filename);
+
+            Download("", filename);
+            
         }
+        private void Download(string folder, string file)
+        {
+            var webclient = new WebClient();
+            webclient.DownloadFile(downloadBase+ folder + file, file);
+        }
+        
+
 
         //patch
         private void button1_Click(object sender, EventArgs e)
         {
+            Download(listFile);
             files = File.ReadAllLines(listFile);
             this.label1.Text = "Начата загрузка файлов\n";
             this.progressBar1.Minimum = 0;
             this.progressBar1.Maximum = files.Length;
+
+            var byondDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\BYOND\\cache";
+            var cacheFolder = Directory.GetDirectories(byondDir).OrderByDescending(dir => Directory.GetLastWriteTime(dir)).First();
+
             foreach (string file in files)
             {
-                Download(file);
+                Download("Translations/", file);
+
+                if (File.Exists(cacheFolder+"\\" + file))
+                    File.Delete(cacheFolder+"\\" + file);
+
+                File.Move(file, cacheFolder+"\\" + file);
+
                 this.label1.Text += file + "\n";
+
                 this.progressBar1.Value++;
             }
-            this.label1.Text += "Закончено";
-
+            this.label1.Text += "Закончено\n";
+            
+            
+            
            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var current = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent;
+            var list = current.GetFiles().Where(file=>file.Name==listFile).First();
+            var translations = current.GetDirectories().Where(dir => dir.Name == "Translations").First().GetFiles().Select(file=>file.Name).ToArray();
+
+            File.WriteAllLines(list.FullName, translations);
+           
+            
+
+
         }
 
        
